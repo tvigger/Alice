06d0ad8ecad311dc3e88b0ddaec2d9fcb9dc6a99
tvigger <tvigmaster@mail.ru>
2024-04-06 13:34:20 +0300

    2

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from flask import Flask, request, jsonify
 import logging
+import requests
+import math
 
 app = Flask(__name__)
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 sessionStorage = {}
 
 
 @app.route('/post', methods=['POST'])
 def main():
-    logging.info(f'Request: {request.json!r}')
+    logging.info('Request: %r', request.json)
     response = {
         'session': request.json['session'],
         'version': request.json['version'],
@@ -16,54 +18,63 @@ def main():
             'end_session': False
         }
     }
-    handle_dialog(request.json, response)
-    logging.info(f'Response:  {response!r}')
+    handle_dialog(response, request.json)
+    logging.info('Response: %r', response)
     return jsonify(response)
 
 
-def handle_dialog(req, res):
-    user_id = req['session']['user_id']
-    if req['session']['new']:
-        sessionStorage[user_id] = {
-            'suggests': [
-                "Не хочу.",
-                "Не буду.",
-                "Отстань!",
-            ]
+def handle_dialog(res, req):
+    pass
+
+
+def get_coordinates(city_name):
+    try:
+        url = "https://geocode-maps.yandex.ru/1.x/"
+        params = {
+            "apikey": "c5885d1c-e551-45a6-8568-d8154540b3ca",
+            'geocode': city_name,
+            'format': 'json'
         }
-        res['response']['text'] = 'Привет! Купи слона!'
-        res['response']['buttons'] = get_suggests(user_id)
-        return
-    if req['request']['original_utterance'].lower() in [
-        'ладно',
-        'куплю',
-        'покупаю',
-        'хорошо'
-    ]:
-        res['response']['text'] = 'Слона можно найти на Яндекс.Маркете!'
-        res['response']['end_session'] = True
-        return
-    res['response']['text'] = \
-        f"Все говорят '{req['request']['original_utterance']}', а ты купи слона!"
-    res['response']['buttons'] = get_suggests(user_id)
+        response = requests.get(url, params)
+        json = response.json()
+        coordinates_str = json['response']['GeoObjectCollection'][
+            'featureMember'][0]['GeoObject']['Point']['pos']
+        long, lat = map(float, coordinates_str.split())
+        return long, lat
+    except Exception as e:
+        return e
 
 
-def get_suggests(user_id):
-    session = sessionStorage[user_id]
-    suggests = [
-        {'title': suggest, 'hide': True}
-        for suggest in session['suggests'][:2]
-    ]
-    session['suggests'] = session['suggests'][1:]
-    sessionStorage[user_id] = session
-    if len(suggests) < 2:
-        suggests.append({
-            "title": "Ладно",
-            "url": "https://market.yandex.ru/search?text=слон",
-            "hide": True
-        })
-    return suggests
+def get_country(city_name):
+    try:
+        url = "https://geocode-maps.yandex.ru/1.x/"
+        params = {
+            "apikey": "c5885d1c-e551-45a6-8568-d8154540b3ca",
+            'geocode': city_name,
+            'format': 'json'
+        }
+        data = requests.get(url, params).json()
+        return data['response']['GeoObjectCollection'][
+            'featureMember'][0]['GeoObject']['metaDataProperty'][
+            'GeocoderMetaData']['AddressDetails']['Country']['CountryName']
+    except Exception as e:
+        return e
 
 
-if __name__ == '__main__':
-    app.run()
+def get_distance(p1, p2):
+    # p1 и p2 - это кортежи из двух элементов - координаты точек
+    radius = 6373.0
+
+    lon1 = math.radians(p1[0])
+    lat1 = math.radians(p1[1])
+    lon2 = math.radians(p2[0])
+    lat2 = math.radians(p2[1])
+
+    d_lon = lon2 - lon1
+    d_lat = lat2 - lat1
+
+    a = math.sin(d_lat / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(d_lon / 2) ** 2
+    c = 2 * math.atan2(a ** 0.5, (1 - a) ** 0.5)
+
+    distance = radius * c
+    return distance
